482ba862e36d0003b5661ae73123b145885b0cb2
UsamaA7med <osamaahmedwork1@gmail.com>
2025-02-27 16:14:17 +0200

    token changes

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -12,10 +12,5 @@ export default async (username, email, fullname, id, res) => {
       expiresIn: "7d",
     }
   );
-  res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    sameSite: "strict",
-    secure: true,
-  });
+  res.cookie("jwt", token);
 };
